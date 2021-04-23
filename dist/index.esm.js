c3089ae6d0db263c6db00573b7e46114e862483e
Sebastian Andil <sebastian.andil@gmail.com>
2021-04-23 16:37:13 +0200

    Fix stuff

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -1,2 +1,2 @@
 import r from"type-detect";function t(t,e){if(!Array.isArray(t))throw TypeError(`I can't work with ${r(t)}`);return t.reduce((r,t,o,i)=>[...r,t,...o+1===i.length?[]:[e]],[])}export{t as intersperse};
-//# sourceMappingURL=index.modern.js.map
+//# sourceMappingURL=index.esm.js.map
--- dist/index.module.js
+++
@@ -1,2 +0,0 @@
-function e(e,n){return e.reduce((e,r,t,u)=>[...e,r,...t+1===u.length?[]:[n]],[])}export{e as intersperse};
-//# sourceMappingURL=index.module.js.map
